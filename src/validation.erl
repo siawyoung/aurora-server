1d97634ee4b3a4f0293f300fc8ba1a6213037ca2
Lau Siaw Young <lausiawyoung@gmail.com>
2015-04-09 09:51:24 +0800

    Remove timestamp validation from auth messages

--- src/validation.erl
+++ src/validation.erl
@@ -19,9 +19,8 @@ validate_and_parse_auth(Socket, RawData) ->
         UserName     = maps:get(username, ParsedJson, missing_field),
         SessionToken = maps:get(session_token, ParsedJson, missing_field),
         PhoneNumber  = maps:get(from_phone_number, ParsedJson, missing_field),
-        TimeStamp    = maps:get(timestamp, ParsedJson, missing_field),
 
-        case ((Type =/= <<"AUTH">>) or (UserName == missing_field) or (SessionToken == missing_field) or (PhoneNumber == missing_field) or (TimeStamp == missing_field)) of
+        case ((Type =/= <<"AUTH">>) or (UserName == missing_field) or (SessionToken == missing_field) or (PhoneNumber == missing_field)) of
 
           true ->
               io:format("Message from validate_auth_message: Invalid auth message~n", []),
